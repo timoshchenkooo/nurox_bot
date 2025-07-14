166226c235dca582bfdb5205aca1569edfbaa903
timoshchenkooo <mebel.horizon@yandex.ru>
2025-07-15 05:15:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-API_URL = "https://Ionit-Nurox.hf.space/generate"
-BOT_TOKEN = "7468794143:AAHnMROybcx5IX1OYuh0ZFtqq5ebAfom7hQ"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("7468794143:AAHnMROybcx5IX1OYuh0ZFtqq5ebAfom7hQ")
+API_URL = os.getenv("https://Ionit-Nurox.hf.space/generate")
